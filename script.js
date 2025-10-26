4214890722695345ccb3fde8c03f6030170bba36
LucaComin <personale.luca.cominelli@gmail.com>
2025-10-26 19:24:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -272,7 +272,7 @@ function loadSampleData() {
 }
 
 // Funzione per popolare il menu a tendina delle pietre
-function populateStoneSelect() {
+window.populateStoneSelect = function populateStoneSelect() {
     const select = document.getElementById('stone-select');
     const currentSelection = select.value;
 
